4c9ba58a91099a87019f4f1f33c4568891df210b
Sor4nex <aleksei.ivanovv11@gmail.com>
2025-03-04 22:15:16 +0300

    feat: solution to the 1 task of knn homework: KD-Tree

---
+++ src/homeworks/knn/kd_tree.py
@@ -0,0 +1,79 @@
+import time
+from dataclasses import dataclass
+from typing import Optional, Union
+
+import numpy as np
+
+
+@dataclass
+class Node:
+    split_axis: int | None = None
+    median: float | None = None
+    left_subtree: Optional[Union["Node", np.ndarray]] = None
+    right_subtree: Optional[Union["Node", np.ndarray]] = None
+
+
+class KDTree:
+    def __init__(self, x: np.array, leaf_size: int) -> None:
+        self.root = self._build_kd_tree(x.copy(), leaf_size)
+
+    def __str__(self) -> str:
+
+        def _make_str_recursively(node: Node | np.ndarray, depth: int = 0) -> str:
+            if isinstance(node, Node):
+                return f"""{"\t" * depth}med: {node.median};
+{"\t" * depth}split: {node.split_axis};
+{"\t" * depth}left subtree: (
+{_make_str_recursively(node.left_subtree, depth + 1) if node.left_subtree is not None else None}
+{"\t" * depth})
+{"\t" * depth}right subtree: (
+{_make_str_recursively(node.right_subtree, depth + 1) if node.right_subtree is not None else None}
+{"\t" * depth})"""
+            return f"{"\t" * depth}{str(node)}"
+
+        return _make_str_recursively(self.root)
+
+    def _build_kd_tree(self, x: np.array, leaf_size: int) -> Node | np.ndarray:
+        if len(x) <= leaf_size:
+            return x
+
+        node = Node()
+        # get axis, whose values has the biggest range
+        node.split_axis = int(np.argmax([max(x[:, i]) - min(x[:, i]) for i in range(len(x[0]))]))
+
+        #sort array by axis, whose values has the biggest range
+        x = x[np.argsort(x[:, node.split_axis])]
+        node.median = x[(len(x) - 1) // 2][node.split_axis] if len(x) % 2 == 0 else (x[len(x) // 2][node.split_axis] + x[len(x) // 2 - 1][node.split_axis]) / 2
+        node.left_subtree = self._build_kd_tree(x[:len(x) // 2], leaf_size)
+        node.right_subtree = self._build_kd_tree(x[len(x) // 2:], leaf_size)
+
+        return node
+
+    def query(self, x: np.ndarray, n_nearest: int = 1) -> np.ndarray:
+
+        def _search_n_nearest(point: np.ndarray, node: Node | np.ndarray, n_nearest: int = 1) -> tuple[list, float]:
+            # if we reached leaf, then we return array of points, sorted by euclidian norm with needed target
+            if isinstance(node, np.ndarray):
+                res = [(elem, np.linalg.norm(point - elem)) for elem in node]
+                res.sort(key=lambda el: el[1])
+                if len(res) > n_nearest:
+                    res = res[:n_nearest]
+                max_norm = float(res[-1][1])
+                return res, max_norm
+
+            # find the closest points recursively, by checking left or right side of hyperplane with respect to median
+            res1 = _search_n_nearest(point, node.right_subtree if point[node.split_axis] >= node.median else node.left_subtree, n_nearest)
+
+            # we should also check other side if there`s not enough points or distance to splitting plane less then
+            # maximum distance between founded neighbours and target
+            if abs(point[node.split_axis] - node.median) < res1[1] or len(res1[0]) < n_nearest:
+                res2 = _search_n_nearest(point, node.left_subtree if point[node.split_axis] >= node.median else node.right_subtree, n_nearest)
+                res = res1[0] + res2[0]
+                res.sort(key=lambda el: el[1])
+                if len(res) > n_nearest:
+                    res = res[:n_nearest]
+                max_norm = float(res[-1][1])
+                return res, max_norm
+            return res1
+
+        return np.array([np.array([neighbour[0] for neighbour in _search_n_nearest(point, self.root, n_nearest)[0]]) for point in x])
---
+++ tests/homeworks/knn/test_kd_tree.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from src.homeworks.knn.kd_tree import *
+import pytest
+
+
+def find_nearest_neighbours_stupid(x: np.ndarray, all_points: np.ndarray, n_nearest: int = 1) -> np.ndarray:
+    res = []
+    for point in x:
+        neighbours = all_points.copy()
+        sorted_indices = np.argsort(np.linalg.norm(point - neighbours, axis=1))
+        res.append(np.array(neighbours[sorted_indices[:n_nearest]]))
+    return np.array(res)
+
+
+def test_kd_tree_query() -> None:
+    for _ in range(100):
+        dim = np.random.randint(1, 20)
+        n_near = np.random.randint(1, 20)
+        x_train = np.array([np.random.uniform(-200, 200, size=dim) for _ in range(np.random.randint(100, 200))])
+        x_test = np.array([np.random.uniform(-200, 200, size=dim) for _ in range(30)])
+
+        tree = KDTree(x_train.copy(), np.random.randint(1, 50))
+        res1 = tree.query(x_test.copy(), n_near)
+        res2 = find_nearest_neighbours_stupid(x_test.copy(), x_train.copy(), n_near)
+
+        assert res1.shape == res2.shape
+        for i in range(len(x_test)):
+            for j in range(n_near):
+                assert np.linalg.norm(x_test[i] - res1[i][j]) == np.linalg.norm(x_test[i] - res2[i][j])
